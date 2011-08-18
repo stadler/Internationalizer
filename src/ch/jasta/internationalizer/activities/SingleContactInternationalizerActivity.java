dfc667ed0a3f39e352a4e95902ff1f7425c1624c
Jacques Stadler <jacques.stadler@gmail.com>
2011-08-19 00:39:15 +0200

    Added more Activities. Started Overview Layout. Removed layout/main.xml

--- src/ch/jasta/internationalizer/activities/SingleContactInternationalizerActivity.java
+++ src/ch/jasta/internationalizer/activities/SingleContactInternationalizerActivity.java
@@ -51,9 +51,7 @@ public class SingleContactInternationalizerActivity extends ListActivity {
 
     super.onCreate(savedInstanceState);
 
-    List<Contact> contactsList = getContacts();
-    Contact[] contactsStringArray = contactsList.toArray(new Contact[contactsList.size()]);
-    setListAdapter(new ArrayAdapter<Contact>(this, R.layout.contact_list_item, contactsStringArray));
+    this.setListAdapter(new ArrayAdapter<Contact>(this, R.layout.contact_list_item, getContacts()));
     
     ListView lv = getListView();
     lv.setTextFilterEnabled(true);
@@ -69,6 +67,32 @@ public class SingleContactInternationalizerActivity extends ListActivity {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
       }
     });
+  }
+
+  @Override
+  protected void onStart() {
+      super.onStart();
+      // The activity is about to become visible.
+  }
+  @Override
+  protected void onResume() {
+      super.onResume();
+      // The activity has become visible (it is now "resumed").
+  }
+  @Override
+  protected void onPause() {
+      super.onPause();
+      // Another activity is taking focus (this activity is about to be "paused").
+  }
+  @Override
+  protected void onStop() {
+      super.onStop();
+      // The activity is no longer visible (it is now "stopped")
+  }
+  @Override
+  protected void onDestroy() {
+      super.onDestroy();
+      // The activity is about to be destroyed.
   }
   
   private List<Contact> getContacts() {
